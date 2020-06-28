7dd81adcf51f8389c049eec8734fffcabb670c93
Kristijan Kralj <kristijan.kralj@live.com>
2020-06-28 14:09:43 +0200

    Implement register view

--- Cryptollet/Modules/Login/LoginViewModel.cs
+++ Cryptollet/Modules/Login/LoginViewModel.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Cryptollet.Common.Base;
+using Cryptollet.Common.Navigation;
+using Cryptollet.Modules.Register;
+using Xamarin.Forms;
 
 namespace Cryptollet.Modules.Login
 {
     public class LoginViewModel: BaseViewModel
     {
+        private INavigationService _navigationService;
 
+        public LoginViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public ICommand RegisterCommand { get => new Command(async () => await GoToRegister()); }
+
+        private async Task GoToRegister()
+        {
+            await _navigationService.PushAsync<RegisterViewModel>();
+        }
     }
 }
--- Cryptollet/Modules/Register/RegisterViewModel.cs
+++ Cryptollet/Modules/Register/RegisterViewModel.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Cryptollet.Common.Base;
+using Cryptollet.Common.Navigation;
+using Xamarin.Forms;
 
 namespace Cryptollet.Modules.Register
 {
     public class RegisterViewModel: BaseViewModel
     {
+        private INavigationService _navigationService;
 
+        public RegisterViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public ICommand LoginCommand { get => new Command(async () => await GoToLogin()); }
+
+        private async Task GoToLogin()
+        {
+            await _navigationService.PopAsync();
+        }
     }
 }
