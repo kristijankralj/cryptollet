f6489f92fa8bc069a8de30b5fc9b0e7c26620882
Kristijan Kralj <kristijan.kralj@live.com>
2020-06-29 22:59:02 +0200

    Register view - add validations

--- Cryptollet/Modules/Register/RegisterViewModel.cs
+++ Cryptollet/Modules/Register/RegisterViewModel.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using Cryptollet.Common.Base;
 using Cryptollet.Common.Navigation;
+using Cryptollet.Common.Validation;
+using Cryptollet.Modules.Wallet;
 using Xamarin.Forms;
 
 namespace Cryptollet.Modules.Register
@@ -14,13 +16,69 @@ namespace Cryptollet.Modules.Register
         public RegisterViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
+            AddValidations();
+        }
+
+        private ValidatableObject<string> _email;
+        public ValidatableObject<string> Email
+        {
+            get => _email;
+            set { SetProperty(ref _email, value); }
+        }
+
+        private ValidatableObject<string> _password;
+        public ValidatableObject<string> Password
+        {
+            get => _password;
+            set { SetProperty(ref _password, value); }
+        }
+
+        private ValidatableObject<string> _name;
+        public ValidatableObject<string> Name
+        {
+            get => _name;
+            set { SetProperty(ref _name, value); }
         }
 
         public ICommand LoginCommand { get => new Command(async () => await GoToLogin()); }
+        public ICommand RegisterUserCommand { get => new Command(async () => await RegisterUser()); }
+
+        private async Task RegisterUser()
+        {
+            IsBusy = true;
+            if (AreEntriesCorrectlyPopulated())
+            {
+                await _navigationService.InsertAsRoot<WalletViewModel>();
+            }
+            IsBusy = false;
+        }
 
         private async Task GoToLogin()
         {
             await _navigationService.PopAsync();
+        }
+
+        private void AddValidations()
+        {
+            _email = new ValidatableObject<string>();
+            _password = new ValidatableObject<string>();
+            _name = new ValidatableObject<string>();
+
+            _email.Validations.Add(new IsNotNullOrEmptyRule<string> { ValidationMessage = "Email is empty." });
+            _email.Validations.Add(new EmailRule<string> { ValidationMessage = "Email is not in correct format." });
+
+            _password.Validations.Add(new IsNotNullOrEmptyRule<string> { ValidationMessage = "Password is empty." });
+
+            _name.Validations.Add(new IsNotNullOrEmptyRule<string> { ValidationMessage = "Name is empty." });
+        }
+
+        private bool AreEntriesCorrectlyPopulated()
+        {
+            _email.Validate();
+            _password.Validate();
+            _name.Validate();
+
+            return _email.IsValid && _password.IsValid && _name.IsValid;
         }
     }
 }
